f601292074bf0731cbf26d8b03303a9fecd86d61
najwalutfia <najwalutfianisya@gmail.com>
2024-05-22 19:17:21 +0700

    fitur2

---
+++ src/components/PostMenu.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="nav-link" :class="{ active: currentView === 'posts' }" @click="changeView('posts')">Posts</button>
+</template>
+
+<script>
+export default {
+  props: ['currentView'],
+  methods: {
+    changeView(view) {
+      this.$emit('change-view', view);
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* CSS khusus untuk komponen PostMenu */
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+    <button class="nav-link" :class="{ active: currentView === 'todos' }" @click="changeView('todos')">Todos</button>
+  </template>
+  
+  <script>
+  export default {
+    props: ['currentView'],
+    methods: {
+      changeView(view) {
+        this.$emit('change-view', view);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* CSS khusus untuk komponen TodosMenu */
+  </style>
+  
